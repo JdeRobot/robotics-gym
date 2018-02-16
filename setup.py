e3f32c44a91d1eaecb5c93efd68c98eb8c2fac20
Alberto Martin <amartin@xactware.com>
2018-02-16 13:43:39 +0100

    Fixed setup.py and added Makefile.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 
-from setuptools import setup
+from setuptools import setup, find_packages
 import sys, os.path
 
 # Don't import gym module here, since deps may not be installed
@@ -7,9 +7,10 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'robotics-gym'))
 
 setup(name='robotics-gym',
       version='0.0.1',
-      install_requires=['gym>=0.2.3'],
-      description='The OpenAI Gym for robotics: A toolkit for developing and comparing your reinforcement learning agents using Gazebo, Player/Satage and ROS.',
-      url='',
+      install_requires=['gym>=0.2.3', 'pyros_setup'],
+      description='The OpenAI Gym for robotics: A toolkit for developing and comparing your reinforcement learning agents using Gazebo, Player/Stage and ROS.',
+      url='https://github.com/JdeRobot/robotics-gym',
       author='JdeRobot',
+      packages = find_packages(),
       package_data={'robotics-gym': ['envs/gazebo/assets/launch/*.launch', 'envs/gazebo/assets/worlds/*']},
 )
